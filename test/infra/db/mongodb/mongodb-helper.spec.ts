5885cea160dd3afdea1b657f766a2185d84a649d
devdbreno <dbrenoti@gmail.com>
2021-06-22 22:13:59 -0300

    chore: change variable name sut > mongodbSut

--- test/infra/db/mongodb/mongodb-helper.spec.ts
+++ test/infra/db/mongodb/mongodb-helper.spec.ts
@@ -2,28 +2,28 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 
 import MongodbHelper from '@infra/db/mongodb/mongodb-helper'
 
-const sut = {
+const mongodbSut = {
   mongodbHelper: new MongodbHelper(),
   mongodbMemoryServer: new MongoMemoryServer()
 }
 
 describe('Mongodb Helper', () => {
   beforeAll(async () => {
-    const mongodbUri = await sut.mongodbMemoryServer.getUri()
-    await sut.mongodbHelper.connect(mongodbUri)
+    const mongodbUri = await mongodbSut.mongodbMemoryServer.getUri()
+    await mongodbSut.mongodbHelper.connect(mongodbUri)
   })
 
   afterAll(async () => {
-    await sut.mongodbHelper.disconnect()
+    await mongodbSut.mongodbHelper.disconnect()
   })
 
   it('Should reconnect if mongodb is down.', async () => {
-    let userCollection = await sut.mongodbHelper.getCollection('users')
+    let userCollection = await mongodbSut.mongodbHelper.getCollection('users')
     expect(userCollection).toBeTruthy()
 
-    await sut.mongodbHelper.disconnect()
+    await mongodbSut.mongodbHelper.disconnect()
 
-    userCollection = await sut.mongodbHelper.getCollection('users')
+    userCollection = await mongodbSut.mongodbHelper.getCollection('users')
     expect(userCollection).toBeTruthy()
   })
 })
