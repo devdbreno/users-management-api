d862958e58a919539ca0c4a84d2d13b19056558f
devdbreno <dbrenoti@gmail.com>
2021-06-23 21:27:31 -0300

    chore(test): ensure POST /api/user returns

--- test/main/routes/user-routes.test.ts
+++ test/main/routes/user-routes.test.ts
@@ -1,13 +1,14 @@
-import { deepStrictEqual } from 'assert'
+import assert from 'assert'
 
 import request from 'supertest'
 import { Collection } from 'mongodb'
 
 import app from '@main/config/app'
 import { MONGO_URL } from '@main/config/env'
-
 import { genRandomValidUser } from '@test/__fixtures__/domain'
 import { MongodbHelper } from '@infra/db/mongodb/mongodb-helper'
+import { AddUserController } from '@presentation/controllers'
+import { AddUser } from '@domain/usecases'
 
 describe('User Routes', () => {
   let userCollection: Collection
@@ -29,7 +30,20 @@ describe('User Routes', () => {
     const randomValidUser = genRandomValidUser()
 
     it(`Should return 201 'Created' when creating user`, async () => {
-      await request(app).post('/api/user').send(randomValidUser).expect(201)
+      await request(app)
+        .post('/api/user')
+        .send(randomValidUser)
+        .expect(201)
+        .then((response) => {
+          expect(response.statusCode).toEqual(201)
+          expect<AddUserController.Request>({
+            name: response.body.name,
+            lastname: response.body.lastname,
+            nickname: response.body.nickname,
+            address: response.body.address,
+            biography: response.body.biography
+          }).toStrictEqual(randomValidUser)
+        })
     })
 
     it(`Should return 409 'Conflict' when creating user with existing nickname`, async () => {
@@ -41,7 +55,10 @@ describe('User Routes', () => {
         .post('/api/user')
         .send({ ...randomValidUser, nickname: 'torvalds' })
         .expect(409)
-        .expect(({ body: { error } }) => deepStrictEqual(error, 'Nickname is already in use'))
+        .then((response) => {
+          expect(response.statusCode).toStrictEqual(409)
+          expect(response.body.error).toStrictEqual('Nickname is already in use')
+        })
     })
   })
 })
