c5a24d34e1eb81f8c7555bc1ddf9de9f8e6668ed
devdbreno <dbrenoti@gmail.com>
2021-06-25 11:52:49 -0300

    chore: refactor dbAddUserUsecase

--- src/data/usecases/dbAddUser.usecase.ts
+++ src/data/usecases/dbAddUser.usecase.ts
@@ -2,14 +2,14 @@ import { AddUserUsecase } from '@domain/usecases'
 // import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols'
 import { MongodbHelper } from '@infra/db'
 
+import { conflict } from '@presentation/helpers'
 import { buildClientError } from '@presentation/helpers'
 import { NicknameInUseError } from '@presentation/errors'
-import { BuildClientErrorReturn, conflict } from '@presentation/helpers'
 
 export class DbAddUserUsecase implements AddUserUsecase {
   constructor() {} // private readonly checkUserByNicknameRepository: CheckAccountByEmailRepository // private readonly addAccountRepository: AddAccountRepository,
 
-  async add(userData: AddUserUsecase.Params): Promise<AddUserUsecase.Result | BuildClientErrorReturn> {
+  async add(userData: AddUserUsecase.Params): Promise<AddUserUsecase.Return> {
     const userCollection = await MongodbHelper.getCollection('users')
     const existsNickname = await userCollection.findOne({ nickname: userData.nickname }, { projection: { _id: 1 } })
 
@@ -23,6 +23,6 @@ export class DbAddUserUsecase implements AddUserUsecase {
       })
     ).ops
 
-    return MongodbHelper.map(user) as AddUserUsecase.Result
+    return MongodbHelper.map(user) as AddUserUsecase.ResultSucess
   }
 }
