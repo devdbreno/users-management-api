202b729afbdb584eaab713acfb365bfd8443fcd1
devdbreno <dbrenoti@gmail.com>
2021-06-25 17:40:55 -0300

    chore: add noContent http helper function

--- src/presentation/helpers/http.helper.ts
+++ src/presentation/helpers/http.helper.ts
@@ -3,14 +3,19 @@ import { anyData } from '@main/config/app'
 import { ServerError } from '@presentation/errors'
 import { HttpResponse } from '@presentation/protocols'
 
-export const ok = (data: anyData): HttpResponse => ({
+export const ok = <T = anyData>(data: T): HttpResponse => ({
   statusCode: 200,
   body: data
 })
 
-export const created = <T>(data: T): HttpResponse => ({
+export const created = <T = anyData>(data: T): HttpResponse => ({
   statusCode: 201,
   body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
 })
 
 export const conflict = (error: Error): HttpResponse => ({
